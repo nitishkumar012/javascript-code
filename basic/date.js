5aeb42e04166f7eb310d908638c8eec033b51219
NITISH KUMAR <86454418+nitishkumar012@users.noreply.github.com>
2023-11-07 14:16:07 +0000

    date concept

---
+++ basic/date.js
@@ -0,0 +1,16 @@
+// const myDate=new Date();
+// console.log(myDate);
+// console.log(myDate.toString());// ye string me return karega answer Tue Nov 07 2023 13:55:35 GMT+0000
+// console.log(myDate.toDateString());//Tue Nov 07 2023 output
+// console.log(myDate.toLocaleString());//11/7/2023, 2:00:55 PM output
+// console.log(typeof(myDate));//it is an object type
+
+
+// TIME STAMPS IN JAVASCRIPT 
+// let myTime = Date.now();
+// console.log(myTime);//1699366059309 mili second time
+let newDate = new Date();
+// console.log(newDate.getMonth()+1);// in javascript month is actually start from 0 for this confusion and getting the actual value we add +1 
+// console.log(newDate.getDay());//this return current day
+// console.log(newDate.getDate());//this return current date
+
