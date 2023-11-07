71242eaa2068803cdc9d91182e557a88cfe28216
NITISH KUMAR <86454418+nitishkumar012@users.noreply.github.com>
2023-11-07 08:29:55 +0000

    adding datatype file

--- basic/basic.js
+++ basic/basic.js
@@ -1,1 +1,11 @@
 console.log("THIS IS MY FIRST JAVASCRIPT PROGRAM");
+const rollNo = 2002920100051;
+var age = 20;
+let ageNow = "21";
+number="0123456789";
+
+console.table([rollNo,age,ageNow,number]);
+
+let houseNo;
+console.log(houseNo);
+
---
+++ basic/datatype.js
@@ -0,0 +1,13 @@
+"use strict" /* it treeat all the js code as a version */
+
+let number = 12;
+let name = " ";
+isNumber=true;
+// humare pass javascript me bhout sare datatype hai 
+// jinme 
+// number
+// BigInt
+// String
+// Boolean
+// null  it is an type of object
+// undefined
