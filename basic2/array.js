1e01f4d27d6042d0f75289e4d5b3af98f4f361cc
NITISH KUMAR <86454418+nitishkumar012@users.noreply.github.com>
2023-11-18 07:12:07 +0000

    add

---
+++ basic2/array.js
@@ -0,0 +1,10 @@
+let arr=[1,2,3,4,5];//1st decleration
+// console.log(arr);
+// console.log(arr[0]);
+// let a=new Array(1,2,3,4)//2nd dcleration
+// console.log(a);
+// let b = new Array(1, 2 , "Nitish " , true);// in javascript array is collection of multiple datatype
+// console.log(b);
+arr.push(6);
+console.log(arr);//pushing new element
+
