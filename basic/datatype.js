f58102a5e22383b77c4c37710fde25d565ebdf8a
NITISH KUMAR <86454418+nitishkumar012@users.noreply.github.com>
2023-12-27 06:18:44 +0000

    basic

--- basic/datatype.js
+++ basic/datatype.js
@@ -1,8 +1,8 @@
 "use strict" /* it treeat all the js code as a version */
 
-let number = 12;
-let name = " ";
-isNumber=true;
+// let number = 12;
+// let name = " ";
+// isNumber=true;
 // humare pass javascript me bhout sare datatype hai 
 // jinme 
 // number
@@ -11,3 +11,12 @@ isNumber=true;
 // Boolean
 // null  it is an type of object
 // undefined
+var name='Nitish'  // variable me isko redecleare aur reassign bhi kar sakte hai mtl same name dobara decleare kar sakte hai 
+// var me ye isliye hai q ki previous javascript me (function scope,block scope) naam ki koi chej nhi thi
+let sname='Kumar'
+//let ko hum redecleare nhi kar sakte hai mgr reassign kar sakte hai
+const fname='Nitish kumar'
+//const ko na hum redecleare kar sakte hai na reassign 
+console.table([name,sname,fname]);
+// isme hum agar koi value assign nhi karte hai to usko null kar sakte hai
+//ek isme undefined bhi hotta hai agar hum kuch assign hi na kare to undefined ho jat hai
