e81689150a12c64ba26dcba03102d7e58d1de8f3
NITISH KUMAR <86454418+nitishkumar012@users.noreply.github.com>
2023-12-28 07:20:32 +0000

    edit

--- basic/function.js
+++ basic/function.js
@@ -10,7 +10,7 @@
 // console.log(str.lastIndexOf(val));
 // }
 // last('i');
-
+//************************************************************************************************************ */
 //function that using variable
 
 // let val=function(str){
@@ -18,7 +18,7 @@
 // }
 // val('nitish kumar');
 
-
+//********************************************************************************************************* */
 //Arrow Function is an special type function that will use when we want fast execution 
 
 // let val=(name)=>{
@@ -26,12 +26,11 @@
 // }
 // let res=val('NITISH KUMAR');
 // console.log(res);
-
+//********************************************************************************************* */
 //split
 // let str="Nitish Kumar";
 // console.log(str.split(" "));
- 
-
+ //******************************************************************************************** */
 //Slice
 // function s(name){
 //     console.log(name.slice(0,5));
@@ -44,7 +43,7 @@
 // console.log(val.length);
 // let val2=s(val);
 // console.log(val2.length);
-
+//************************************************************************************************ */
 //higher order function mtl function as a arrgument pass karna
 
 // let upper=(str)=>{
@@ -58,7 +57,7 @@
 // }
 // console.log(high('nitish',upper))
 // console.log(high("NITISH",lower));
-
+//********************************************************************************************* */
 // another type of higher order function is Function passing as a function arrgument meanst returning an function as arrugument
 
 // let con=function(msg){
@@ -68,13 +67,13 @@
 // }
 // let com=con('I love you');
 // com('Babu');
-
-//                                Immediately Invoked function Expression : This is an function when we want to exection of function fast
+//***************************************************************************************** */
+//Immediately Invoked function Expression : This is an function when we want to exection of function fast
 
 // (function(name){
 //     console.log(`I love mera Shona Babu ${name}`)
 // })('Vishu');
-
+//*********************************************************************************************** */
 // Hoisting in javascript this is basically for function and var keyword
 //Hoisting is defined as technique that will decleare function aur variable to the top of code
 // test=10;
