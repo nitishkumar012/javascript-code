4baf539760f72693f17dd2f7d01f1833b17cdb9f
NITISH KUMAR <86454418+nitishkumar012@users.noreply.github.com>
2023-12-28 07:44:35 +0000

    normal object things

---
+++ object/object.js
@@ -0,0 +1,24 @@
+"use strict"
+//****************************Intro Of Object****************************************************** //
+// obect is like a combination of key and value
+let car={
+    name:'Mechlarin',
+    model:'2020',
+    color:'Black',
+    spped:'480mh'
+}
+// console.log(typeof(car));
+// console.log(car);
+// console.log(car['name']);
+// car['name']='Gwagan'
+// console.log(car)
+// console.log(car['name']);
+ 
+//*************************************if we want to delete some thing in object*************************************//
+
+// delete car.name;
+// console.log(car);
+//****************************************Adding new thing in object*************************************//
+
+// car.name='Audi Q8';
+// console.log(car);
